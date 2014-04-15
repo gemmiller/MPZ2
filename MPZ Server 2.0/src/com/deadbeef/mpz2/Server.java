a29e50c6926c488065c864a50327c43f3f60b4cc
gemmiller <gemiller@iastate.edu>
2014-04-15 13:23:22 -0500

    Server updates

--- MPZ Server 2.0/src/com/deadbeef/mpz2/Server.java
+++ MPZ Server 2.0/src/com/deadbeef/mpz2/Server.java
@@ -1,8 +1,16 @@
 package com.deadbeef.mpz2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.firebase.client.GenericTypeIndicator;
 import com.firebase.client.ValueEventListener;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
@@ -14,36 +22,73 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 public class Server {
 	static HtmlInput input;
 	static HtmlInput submit;
+	static ArrayList<String> commands = new ArrayList<String>();
 	public static void main(String[] args)
 	{
+		HtmlPage page = null;
+		final WebClient webClient = new WebClient();
 		
-		Firebase ref = new Firebase("https://brilliant-fire-4503.firebaseio.com/");
-
-		// Write data to Firebase
-		
-		 final WebClient webClient = new WebClient();
+		//connect to Zork
 		 try{
-		    HtmlPage page = webClient.getPage("http://www.web-adventures.org/cgi-bin/webfrotz?s=ZorkDungeon&n=5567");
-		    System.out.println(page.asText().replace("Enter", ""));
-		 // Get the form that we are dealing with and within that form, 
-		    // find the submit button and the field that we want to change.
-		    final HtmlForm form = page.getFormByName("f");
-
-		    final HtmlSubmitInput button = form.getInputByValue("Enter");
-		    final HtmlTextInput textField = form.getInputByName("a");
-		    textField.setValueAttribute("go west");
-
-		    // Now submit the form by clicking the button and get back the second page.
-		    page = button.click();
-		    //send data to firebase
-		    ref.child("zorktext").setValue(page.getBody().asText().replace("Enter", ""));
-		    System.out.println(page.getBody().asText().replace("Enter", ""));
+			 page = webClient.getPage("http://www.web-adventures.org/cgi-bin/webfrotz?s=ZorkDungeon&n=29262");
 		 }
 		    
 		 catch(Exception e)
 		 {
 			 System.out.println(e.getMessage());
 		 }
+		Firebase ref = new Firebase("https://brilliant-fire-4503.firebaseio.com/");
+		Firebase comRef = ref.child("command");
+		Firebase zorkRef = ref.child("zorktext");
+		 zorkRef.setValue(page.getBody().asText().replace("Enter", ""));
+		
+		
+		
+		//add command listener events
+		// Read data and react to changes
+		comRef.addValueEventListener(new ValueEventListener() {
+
+		    @Override
+		    public void onDataChange(DataSnapshot snap) {
+		    	String com = (String)snap.getValue();
+		        if(com!=null&&!com.trim().isEmpty())
+		        {
+		        	commands.add(com);
+		        }
+		    }
+
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});	
+			//update page loop
+			while(true)
+			{
+				
+				try{
+				// Get the form that we are dealing with and within that form, 
+			    // find the submit button and the field that we want to change.
+					if(!commands.isEmpty()){
+					    final HtmlForm form = page.getFormByName("f");
+		
+					    final HtmlSubmitInput button = form.getInputByValue("Enter");
+					    final HtmlTextInput textField = form.getInputByName("a");
+					    textField.setValueAttribute(commands.remove(0));
+		
+					    // Now submit the form by clicking the button and get back the second page.
+					    page = button.click();
+					    zorkRef.setValue(page.getBody().asText().replace("Enter", ""));
+					}
+				}
+				catch(Exception e)
+				{
+					break;
+				}
+			}
+		
+		 
 		    webClient.closeAllWindows();
 		    System.exit(0);
 	}
